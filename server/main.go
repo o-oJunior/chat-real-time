9675ab69bc2ed8234363d180f42d96ff019103c8
Édio Júnior <ediomendesjunior@hotmail.com>
2024-07-15 22:18:03 -0300

    chore: added cors and crypto

--- server/main.go
+++ server/main.go
@@ -4,22 +4,32 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"server/src/config/logger"
+	"server/src/config"
 	"server/src/router"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	logger := config.NewLogger("server")
 	err := godotenv.Load()
 	if err != nil {
-		logger.Error("[ENV] Erro ao carregar as variaveis de ambiente!", err, true)
+		logger.Error("Erro ao carregar as variaveis de ambiente: %v", err)
+		panic(err)
 	}
 	port := fmt.Sprintf(":%s", os.Getenv("ENV_PORT"))
-	logger.Info("[SERVER] Sucesso ao iniciar o servidor na porta %s", port)
+	logger.Info("Sucesso ao iniciar o servidor na porta %s", port)
 	gin.DefaultWriter = io.Discard
 	rt := gin.Default()
+	rt.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // Permitir todos os domínios
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
 	userGroup := rt.Group("/api/user")
 	router.UserRouters(userGroup)
 	rt.Run(port)
