360cc8353d94ac44e46b0f4803ada26ceab337a5
Édio Júnior <ediomendesjunior@hotmail.com>
2024-07-30 18:42:27 -0300

    feat: include providers redux

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,5 +1,20 @@
-import type { AppProps } from "next/app";
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import Layout from "./layout"
+import Head from "next/head"
+import Providers from "../redux/providers"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Providers>
+        <Head>
+          <link rel="icon" href="/gopher.png" />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Providers>
+    </>
+  )
 }
