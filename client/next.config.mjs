d012a1b450c22763abb5be482e57493593a9709c
Édio Júnior <ediomendesjunior@hotmail.com>
2024-07-30 18:48:02 -0300

    config dotenv

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+import dotenv from "dotenv"
+dotenv.config({ path: "../.env" })
 const nextConfig = {
   reactStrictMode: true,
-};
+}
 
-export default nextConfig;
+export default nextConfig
